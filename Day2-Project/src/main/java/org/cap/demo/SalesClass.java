c92cd437461e4c186d7f8712a5f1afb306928864
Training2017 <vidavid@LIN73000499.corp.capgemini.com>
2018-03-30 12:46:11 +0530

    enhanced for loop with sum

--- Day2-Project/src/main/java/org/cap/demo/SalesClass.java
+++ Day2-Project/src/main/java/org/cap/demo/SalesClass.java
@@ -4,6 +4,12 @@ public class SalesClass {
 	
 	public void show(){
 		int[] arg={12,45,67,90};
+		int sum=0;
+		for(int num:arg){
+			sum+=num;
+		}
+		
+		System.out.println("Summation: " + sum);
 		
 		System.out.println("Show Method");
 		System.out.println("Show Method for SalesClss");
